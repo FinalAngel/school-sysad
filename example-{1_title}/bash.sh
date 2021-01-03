6486c3c04b416c30ba7180937ac401168d6663a0
Dario Voser <dario.voser@hispeed.ch>
2021-01-03 18:39:42 +0100

    update Bash Script

--- example-{1_title}/bash.sh
+++ example-{1_title}/bash.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-time=$(date +"%H")
-date=$(date '+%A the +%m.%d.%Y - %H:%M"')
-afternoon=15
-URL='http://api.openweathermap.org/data/2.5/weather?q=Zurich&appid=a98b12db9d2e7a5bf4c42f264289e7f3&units=metric'
 
-#Check if morning or evening
+# print greeting
+time=$(date +"%H")
+afternoon=15
 if [ $time -gt $afternoon ]
  then
     echo "Good Afternoon/Evening $USER"
@@ -12,7 +10,13 @@ else
     echo "Good Morning $USER"
 fi   
 
-#Output the current weather 
+# print date information
+date=$(date '+%A the +%m.%d.%Y - %H:%M"')
+echo $date
+
+# print weather information 
+URL='http://api.openweathermap.org/data/2.5/weather?q=Zurich&appid=a98b12db9d2e7a5bf4c42f264289e7f3&units=metric'
+
 declare -A icons=( ["Clouds"]="☁️ " ["Thunderstorm"]="⚡ " ["Drizzle"]="⛆ " ["Rain"]="☔ " ["Snow"]="❄️ ️" ["Mist"]="🌫️ " ["Fog"]="🌫️ ️" ["Clear"]="☀️ " )
 
 data=$(curl -s $URL)
@@ -24,7 +28,7 @@ description=$(echo $data | jq -r '.weather[0].description')
 weatherIcon=${icons[$weather]}
 echo "Todays weather is $description $weatherIcon with a temperature of $degrees°C"
 
-#launch wanted programms
+# launch programms
 if [ $time -le $afternoon ]
   then 
     firefox
